a14fdb83c578cc1168c5075aebe881357e4f1f51
tomemexendo <tomemexendo@gmail.com>
2026-05-01 09:24:31 -0300

    Add files via upload

---
+++ next.config.ts
@@ -0,0 +1,7 @@
+import type { NextConfig } from "next";
+
+const nextConfig: NextConfig = {
+  allowedDevOrigins: ['3000-i4zax7f3boch1sbcapmpb-40ab5cb9.us2.manus.computer'],
+};
+
+export default nextConfig;
