27bdd401fe3d810a46d54fbd0230d4fdea2fbca9
tomemexendo <tomemexendo@gmail.com>
2026-05-01 09:45:14 -0300

    Delete next.config.ts

--- next.config.ts
+++
@@ -1,7 +0,0 @@
-import type { NextConfig } from "next";
-
-const nextConfig: NextConfig = {
-  allowedDevOrigins: ['3000-i4zax7f3boch1sbcapmpb-40ab5cb9.us2.manus.computer'],
-};
-
-export default nextConfig;
